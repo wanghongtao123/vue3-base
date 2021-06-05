a440d08d270b295d592a3e5d29e9db14f9b91296
王宏韬 <wanghongtaoaaa@qq.com>
2021-06-05 21:07:01 +0800

    lint

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,37 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
-const Home = () => import("@/views/home/index");
+export const constantRoutes = [
+  // {
+  //   path: "/login",
+  //   component: () => import("@/views/login"),
+  //   hidden: true,
+  // },
+  {
+    path: "/403",
+    name: "403",
+    component: () => import("@/views/403.vue"),
+    hidden: true,
+  },
+  {
+    path: "/404",
+    name: "404",
+    component: () => import("@/views/404.vue"),
+    hidden: true,
+  },
+];
 
+const Home = () => import("@/views/home/index");
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
-    component: Home
-  }
+    component: Home,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 });
 
 export default router;
