3d2fabfa8b2497f1ccc205c272a08c27457b45db
wanghongt123 <9340561+wanghongt123@user.noreply.gitee.com>
2021-07-10 10:25:33 +0800

    feat: 添加通用style

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
-
 <style lang="less">
+@import "./style/index.less";
+
+* {
+  box-sizing: border-box;
+}
+
+body,
+html {
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
